ba45e7037b7eadea673fef69c207205114304d1c
h-y-it <109745891+h-y-it@users.noreply.github.com>
2025-07-13 11:41:26 +0900

    Add files via upload

---
+++ ScreenShot_Test_12.3_THI_RHD.bat
@@ -0,0 +1,14 @@
+@echo off
+
+call ./ScreenShot_Test_Home_PK_Touch_Home_12.3_THI_RHD.bat
+
+call ./ScreenShot_Test_Home_PK_Touch_Phone_12.3_THI_RHD.bat
+
+call ./ScreenShot_Test_Home_PK_Touch_Audio_12.3_THI_RHD.bat
+
+call ./ScreenShot_Test_Home_PK_Touch_AppsInfo_12.3_THI_RHD.bat
+
+call ./ScreenShot_Test_Home_PK_Touch_AllSettings_12.3_THI_RHD.bat
+
+pause
+
